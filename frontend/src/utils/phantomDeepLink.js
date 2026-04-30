5cdb6203a24d164884c8acbc0ea1e662a391d55b
Sewang Rai <sewangsampang0.2@gmail.com>
2026-04-30 21:23:21 +0545

    debug: type check inputs in decryptPayload

--- frontend/src/utils/phantomDeepLink.js
+++ frontend/src/utils/phantomDeepLink.js
@@ -29,6 +29,10 @@ export function buildSignURL(dappPublicKey, redirectURL, transaction){
 }
 
 export function decryptPayload(data, nonce, phantomPublicKey, secretKey){
+    if (typeof data !== 'string') throw new Error('data is' + typeof data + ': ' + JSON.stringify(data).slice(0, 30));
+    if (typeof nonce !== 'string') throw new Error('nonce is ' + typeof nonce);
+    if (typeof phantomPublicKey !== 'string') throw new Error ('phantomKey is ' + typeof phantomPublicKey);
+    console.log('types inside decrypt:', typeof data, typeof nonce, typeof phantomPublicKey, typeof secretKey)
     const decodeData = bs58.decode(data);
     const decodeNonce = bs58.decode(nonce);
     const decodePhantomPublicKey = bs58.decode(phantomPublicKey);
